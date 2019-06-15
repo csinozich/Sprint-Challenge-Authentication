af204c76b9961d9e5b576c363352ae3247e779fb
Claire Sinozich <claire.sinozich@gmail.com>
2019-06-15 11:03:39 -0600

    lots of react app set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h2>dad jokes?!</h2>
+      <NavBar />
     </div>
   );
 }
